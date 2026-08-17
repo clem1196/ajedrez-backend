52f94e3bb646e8f78387131eb417f51bad77f26b
clem1196 <clementedela@hotmail.com>
2026-08-17 15:55:02 -0500

    poniendo syncronize:true momentaneamente

--- src/config/dataSource.ts
+++ src/config/dataSource.ts
@@ -11,7 +11,7 @@ export const AppDataSource = new DataSource({
   username: process.env.DB_USER || 'root',
   password: process.env.DB_PASSWORD || 'c1l2e3m1196',
   database: process.env.DB_NAME || 'ajedrez_db',
-  synchronize: false, // 💡 Auto-crea o actualiza las tablas al levantar el servidor (Ideal para desarrollo)
+  synchronize: true, // 💡 Auto-crea o actualiza las tablas al levantar el servidor (Ideal para desarrollo)
   logging: false,
   entities: [User, UserStats, GameHistory],
   ssl: {
